80633d852103dd4fbccd13e699a307faf2435e4b
eyabc <bey4314@naver.com>
2024-05-07 22:31:45 +0900

    add react app

---
+++ react-app/src/App.tsx
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+
+// @ts-ignore
+import React from "react"
+import { useMediaQuery } from "react-responsive"
+
+
+
+function App() {
+
+  const isPc = useMediaQuery({
+    query : "(min-width:1024px)"
+  });
+  const isTablet = useMediaQuery({
+    query : "(min-width:768px) and (max-width:1023px)"
+  });
+  const isMobile = useMediaQuery({
+    query : "(max-width:767px)"
+  });
+
+  return isPc &&(
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
