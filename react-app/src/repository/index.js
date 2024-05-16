694c47993a839f48cf409041d8386b35b1167fca
eyabc <bey4314@naver.com>
2024-05-16 23:09:34 +0900

    토큰 수정

--- react-app/src/repository/index.js
+++ react-app/src/repository/index.js
@@ -7,7 +7,7 @@ export const fetchAll = async () => {
       {
         headers: {
           'Authorization':
-              `Bearer ${"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"}`,
+              `Bearer ${process.env.GITHUB_TOKEN}`,
           Accept: 'application/vnd.github+json',
           "X-GitHub-Api-Version": "2022-11-28"
         },
